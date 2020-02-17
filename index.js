8ae88bfc814a0089acc3c91d7c29536ade856246
petrinivictor <ciurceadaniel@gmail.com>
2020-02-17 23:26:02 +0200

    minor

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-let person ={
 
-     name:'dani',
-     age:20
-
-
-};
 $(document).ready(function(){
    
   $("input").focus(function(){
+        $(this).css("background-color","red");
 
-    $(this).css("background-color","red");
-
-  });
-   $("input").mouseleave(function(){
-
-      $(this).css("background-color","white");
-
-   });
-});
-$("button").click(function(){
-
-     $(this).hide();
-
-});
-
-$(document).ready(function(){
-    
-     $("form").submit(function(event){
-          
-          
-           event.preventDefault();
-
-      $("#p1").text("fnfnfnf");
-
-
-
-          
+     });
+   
+  $("input").mouseleave(function(){
+        $(this).css("background-color","white");
      });
 
+  $("button").click(function(){
+          $(this).hide();
+     });
 
-
+     $("form").submit(function(event){
+          event.preventDefault();
+          $("#p1").text("fnfnfnf");
+     });
 });
+  
+
